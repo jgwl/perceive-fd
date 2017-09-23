ac30d6ea6caac1f1504d81b77e0f187f04a8bd60
jarrettl <jarrettl@umd.edu>
2017-09-23 13:48:07 +0000

    Initial commit: working raw crawler

---
+++ fd_crawler_raw.py
@@ -0,0 +1,137 @@
+import calendar
+import os
+import random
+import re
+import requests
+import time
+
+def dl_range(year_start, month_start, year_end, month_end):
+    """
+    Download multiple months, using start & end year/months.
+    See dl_month for contents.
+    
+    Parameters
+    ----------
+    year_start : int
+    month_start : int (Jan = 1)
+    year_end : int
+    month_end : int (Jan = 1)
+    """
+    
+    ym_start = (year_start * 12) + month_start - 1
+    ym_end = (year_end * 12) + month_end - 1
+    for ym in range(ym_start, ym_end + 1):
+        y, m = divmod(ym, 12)
+        dl_month(y,m+1)
+
+    
+def dl_month(year, month):
+    """
+    Download entire raw html contents of month.
+    Contents will be written to subdir, e.g. ./2017_01
+    
+    Parameters
+    ----------
+    year : int
+    month : int (Jan = 1)
+    """
+    
+    year_str = str(year)
+    month_0x = str(month).zfill(2)
+    month_str = calendar.month_abbr[month]
+    
+    output_path = './' + year_str + '_' + month_0x
+    create_dir(output_path)
+    
+    index_file = dl_index(year_str, month_str, output_path)
+    message_count = parse_index_num(index_file)
+
+    for i in range(message_count):
+        dl_message(year_str, month_str, str(i), output_path)
+        #use random sleep to prevent banning
+        time.sleep(20 * random.random())
+
+
+def dl_index(year, month, path):
+    """
+    Download month's index file.
+    
+    Parameters
+    ----------
+    year : str, e.g. "2017"
+    month : str, e.g., "Jan"
+    path : str
+    
+    Returns
+    -------
+    str
+    
+    """
+    
+    #ex: http://seclists.org/fulldisclosure/2017/Jan/index.html
+    url = 'http://seclists.org/fulldisclosure/' + year + '/' + month + '/index.html'
+    print(url)
+    r = requests.get(url)
+    
+    #save file, e.g.: 2017_Jan.raw.html
+    filename = os.path.join(path, year + '_' + month + '.raw.html')
+    with open(filename, 'w') as w:
+        w.write(r.text)
+
+    return filename
+
+
+def parse_index_num(filename):
+    """
+    Determine how many messages the index file contains. 
+    Parses the first line (e.g., <!-- SecLists-Message-Count: 108 -->)
+    
+    Parameters
+    ----------
+    filename : str
+    
+    Returns
+    -------
+    int
+    
+    """
+    with open(filename, 'r') as f:
+        first_line = f.readline()
+    
+    rx = re.compile('SecLists-Message-Count: ([\d]+)')
+    m = rx.search(first_line)
+    return int(m.group(1))
+
+
+def dl_message(year, month, id, path):
+    """
+    Download individual message.
+    
+    Parameters
+    ----------
+    year : str, e.g. "2017"
+    month : str, e.g., "Jan"
+    id: str, e.g., "0"
+    path : str
+    
+    """
+    
+    #ex: http://seclists.org/fulldisclosure/2017/Jan/0
+    url = 'http://seclists.org/fulldisclosure/' + year + '/' + month + '/' + id
+    print(url)
+    r = requests.get(url)
+    
+    #save file, ex: 2017_Jan_0.raw.html
+    filename = os.path.join(path, year + '_' + month + '_' + id + '.raw.html')
+    with open(filename, 'w') as w:
+        w.write(r.text)
+
+
+def create_dir(directory):
+    """
+    Create dir if not exists.
+    """
+    
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
